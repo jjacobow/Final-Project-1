b188041fb608b501534af84b551367c7af25c587
Brandon Wu <Brandon Wu@128.4.111.16>
2018-12-05 15:36:30 -0500

    FX Clear and Calculate Button functionality

--- FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
+++ FinalExamProject/src/main/java/pkgApp/controller/RetirementController.java
@@ -1,5 +1,6 @@
 package pkgApp.controller;
 
+//import java.awt.Label;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -7,7 +8,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
 import pkgApp.RetirementApp;
+import pkgCore.Retirement;
 
 public class RetirementController implements Initializable {
 
@@ -16,6 +20,21 @@ public class RetirementController implements Initializable {
 	
 	@FXML
 	private TextField txtYearsToWork;
+	@FXML
+	private TextField txtAnnualReturnWorking;
+	@FXML
+	private TextField txtYearsRetired;
+	@FXML
+	private TextField txtAnnualReturnRetired;
+	@FXML
+	private TextField txtRequiredIncome;
+	@FXML
+	private TextField txtMonthlySSI;
+	@FXML
+	private Label LbLSEM;
+	@FXML
+	private Label LbLNTS;
+	
 	
 
 	public RetirementApp getMainApp() {
@@ -32,8 +51,15 @@ public class RetirementController implements Initializable {
 	
 	@FXML
 	public void btnClear(ActionEvent event) {
-		System.out.println("Clear pressed");
 		
+		LbLSEM.setText("");
+		LbLNTS.setText("");
+		txtYearsToWork.setText("");;
+		txtAnnualReturnWorking.setText("");
+		txtYearsRetired.setText("");
+		txtAnnualReturnRetired.setText("");
+		txtRequiredIncome.setText("");
+		txtMonthlySSI.setText("");
 		//	TODO: Clear all the text inputs
 	}
 	
@@ -41,7 +67,32 @@ public class RetirementController implements Initializable {
 	public void btnCalculate(ActionEvent event) {
 		
 		//	TODO: Call AmountToSave and TotalAmountSaved and populate 
+		try {
+		int iYearsWork = Integer.parseInt(txtYearsToWork.getText());
+		double dAnnualReturnWork = Double.parseDouble(txtAnnualReturnWorking.getText());
+		int iYearsRetired = Integer.parseInt(txtYearsRetired.getText());
+		double dAnnualReturnRetired = Double.parseDouble(txtAnnualReturnRetired.getText());
+		double dSSIAmt = Double.parseDouble(txtMonthlySSI.getText());
+		double dRequiredIncome = Double.parseDouble(txtRequiredIncome.getText());
 		
-	}
+		Retirement rt = new Retirement();
+		rt.setiYearsRetired(iYearsRetired);
+		rt.setdAnnualReturnRetired(dAnnualReturnRetired);
+		rt.setdMonthlySSI(dSSIAmt);
+		rt.setdRequiredIncome(dRequiredIncome);
+		rt.setiYearsToWork(iYearsWork);
+		rt.setdAnnualReturnWorking(dAnnualReturnWork);
+		
+		LbLSEM.setText(String.format("$%.2f",rt.AmountToSave()));
+		LbLNTS.setText(String.format("$%.2f", rt.TotalAmountSaved()*-1));
+		
 	
+	}catch(NumberFormatException e) {
+		LbLSEM.setText("Non Number Input!");
+		LbLNTS.setText("Non Number Input!");
+	}catch(Exception e) {
+		LbLSEM.setText("Error");
+		LbLNTS.setText("Error");
+	}
+	}	
 }
