1715f13913d3ba6611c537ef7a038f1c0c73465e
Brandon Wu <Brandon Wu@128.4.111.16>
2018-12-05 14:24:49 -0500

    Retirement Class and Test complete

---
+++ FinalExamProject/src/test/java/pkgTest/RetirementTest.java
@@ -0,0 +1,49 @@
+package pkgTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import pkgCore.Retirement;
+
+public class RetirementTest {
+
+	@Test
+	public void TestPV() {
+		int iYearsRetired = 20;
+		double dAnnualReturn = 0.02;
+		double dExpectedPV = -1454485.55;
+		double dSSIAmt = 2642;
+		double dRequiredIncome = 10000;
+		Retirement rt = new Retirement();
+		rt.setiYearsRetired(iYearsRetired);
+		rt.setdAnnualReturnRetired(dAnnualReturn);
+		rt.setdMonthlySSI(dSSIAmt);
+		rt.setdRequiredIncome(dRequiredIncome);
+		
+		assertEquals(dExpectedPV,rt.TotalAmountSaved(),0.05);
+	}
+	@Test
+	public void TestPMT() {
+		double ExpectedPMT = 554.13;
+		int iYearsWork = 40;
+		double dAnnualReturnWork = 0.07;
+		int iYearsRetired = 20;
+		double dAnnualReturn = 0.02;
+		double dExpectedPV = -1454485.55;
+		double dSSIAmt = 2642;
+		double dRequiredIncome = 10000;
+		
+		Retirement rt = new Retirement();
+		rt.setiYearsRetired(iYearsRetired);
+		rt.setdAnnualReturnRetired(dAnnualReturn);
+		rt.setdMonthlySSI(dSSIAmt);
+		rt.setdRequiredIncome(dRequiredIncome);
+		rt.setiYearsToWork(iYearsWork);
+		rt.setdAnnualReturnWorking(dAnnualReturnWork);
+	
+		assertEquals(dExpectedPV,rt.TotalAmountSaved(),0.05);
+		assertEquals(ExpectedPMT, rt.AmountToSave(),0.05);
+	}
+
+}
